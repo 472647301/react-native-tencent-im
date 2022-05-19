3cb6aaaf56ffb927787540c0cef3ecc01f3b0cc5
秃尾巴的猫 <472647301@qq.com>
2022-05-20 00:26:13 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -125,6 +125,9 @@ declare module "@byron-react-native/tencent-im" {
     textElem: { text: string };
 
     /// 消息类型 为 V2TIM_ELEM_TYPE_CUSTOM，customElem 会存储自定义消息内容
+    /**
+     * '{ NativeMap: {"abc":"123"} }'
+     */
     customElem: any;
 
     /// 消息类型 为 V2TIM_ELEM_TYPE_IMAGE，imageElem 会存储图片消息内容
